bf398ca56027ed4a1943296e565d879fb25ca859
Firefly4266 <gaines.johnp@gmail.com>
2017-09-04 22:35:11 -0700

    Addition exercise complete

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,6 +10,16 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number: ");
+            string userInput = Console.ReadLine();
+            int number1 = int.Parse(userInput);
+
+            Console.Write("Enter a second number: ");
+            userInput = Console.ReadLine();
+            int number2 = int.Parse(userInput);
+
+            int sum = number2 + number1;
+            Console.WriteLine("The sum is {0}", sum); 
         }
     }
 }
